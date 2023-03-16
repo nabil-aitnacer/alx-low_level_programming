d45b791fc78fe69f08bb3f8b4c140b28a9a3c968
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-16 14:17:33 -0700

    add description to main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-/*C program that prints puts  */
 #include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
