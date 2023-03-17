d69ed8c540f7e5eebe90a245b566083c48c09feb
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-17 15:04:46 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char ch = 1;
+	int ch = 0;
 
-	for (ch = '0'; ch <= '9'; ch++)
-		putchar(ch);
+	for (ch = 0; ch < 10; ch++)
+		putchar('0' + ch);
 	putchar('\n');
 	return (0);
 }
