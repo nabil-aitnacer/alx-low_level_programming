09a4eb1ce7ac04daa624d6e7a4bd0f361e82f717
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-21 10:25:31 -0700

    betty funcking style fix it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,17 @@
 
 void print_alphabet_x10(void)
 {
-        char c = 'a';
-        int i = 1;
-	for (i = 1; i <= 10 ; i++)
-        {
-                for ( c = 'a'; c <= 'z'; c++)
-                {
-                        _putchar(c);
-      		}
-                _putchar('\n');
-			}
+	char c = 'a';
+	int a = 1;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+
+	}
+
 }
