d9537da1c199658211ea6a982e820da7b846fb4f
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-21 10:08:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void print_alphabet_x10(void)
+{
+        char c = 'a';
+        int i = 1;
+	for (i = 1; i <= 10 ; i++)
+        {
+                for ( c = 'a'; c <= 'z'; c++)
+                {
+                        _putchar(c)
+      		}
+                _putchar('\n')
+			}
+}
