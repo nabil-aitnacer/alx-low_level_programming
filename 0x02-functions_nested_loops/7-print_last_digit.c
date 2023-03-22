822b3297b73731f3f6bc6fae292714f89bd8cfe2
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-22 04:24:13 -0700

    int print_last_digit(int)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_last_digit - a function that computes the absolute value of an integer
+ *@n : number
+ *Return: last number
+ */
+
+int print_last_digit(int n)
+{
+	int a = n % 10;
+
+	_putchar(a);
+
+	return (a);
+}
