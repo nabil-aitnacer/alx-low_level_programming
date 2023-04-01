38949e5a44a329997c1969828311ae7370148819
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-31 18:12:09 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int  j = 0;
 
-	while (src[j] != '\0' && j < n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
-
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 
 }
