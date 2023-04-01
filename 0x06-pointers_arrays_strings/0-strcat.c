2a0123e643b66ad0674127f1161aa76b4b07414c
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-31 17:12:41 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,34 +9,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int destLen = 0;
-	int srcLen = 0;
-	int i, j;
+	int i = 0, j = 0;
 
-	while (dest[destLen] != '\0')
+	while (dest[i] != '\0')
 	{
-		destLen++;
+		i++;
 	}
 
-	while (src[srcLen] != '\0')
+	while (src[j] != '\0')
 	{
-		srcLen++;
+		desc[i] = src[j];
+		j++;
+		i++;
 	}
+	dest[i] = '\0';
 
-	int len = destLen + srcLen + 1;
+	return (dest);
 
-	char *temp[len];
-
-	for (i = 0; i < destLen; i++)
-	{
-		temp[i] = dest[i];
-	}
-	for (j = 0; j < srcLen; j++)
-	{
-		temp[i++] = src[j];
-	}
-
-	temp[len - 1] = '\0';
-
-	return (temp);
 }
