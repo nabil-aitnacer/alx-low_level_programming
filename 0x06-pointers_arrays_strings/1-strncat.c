ad523ca5380bfe4adf2f865b598e61e6e506bb10
aitnacer-nabil <palancia.gor@gmail.com>
2023-03-31 17:21:16 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest : string
+ * @src : string
+ * @n : n bytes from src
+ *Return: string concatenates.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while ( j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
+}
