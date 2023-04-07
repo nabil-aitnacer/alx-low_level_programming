7d9d941c57aea8f2c4048546f8c17145e2478eae
aitnacer-nabil <palancia.gor@gmail.com>
2023-04-06 18:08:21 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion(char *s) - a function that returns the length of a string.
+ * _strlen_recursion - a function that returns the length of a string.
  * @s: input
  * Return: length of string
  */
@@ -9,9 +9,9 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 +  _strlen_recursion(s + 1);
+		return (1 +  _strlen_recursion(s + 1));
 	}
 
-	return 0;
+	return (0);
 
 }
