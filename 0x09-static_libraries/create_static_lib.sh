9f4591e29b5d0fdc255c602c73b6381dd624d5fd
aitnacer-nabil <palancia.gor@gmail.com>
2023-04-10 07:24:14 -0700

    bash scrpt

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile all the .c files into object files
+gcc -c *.c
+
+# Create the static library from the object files
+ar rcs liball.a *.o
+
+# Clean up the object files
+rm *.o
